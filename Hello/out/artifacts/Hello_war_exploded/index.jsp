b7059a2f5917775720f6ec50f4c5e5264dfd09d8
liuyang99202 <39298580@qq.com>
2018-02-24 17:24:10 +0800

    第一次提交

---
+++ Hello/out/artifacts/Hello_war_exploded/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: lenovo
+  Date: 2018/2/24
+  Time: 15:04
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>首页</title>
+  </head>
+  <body>
+  <h1 style="color:red">hellowhellow </h1>
+  </body>
+</html>
---
+++ Hello/src/Aaaa.java
@@ -0,0 +1,30 @@
+public class Aaaa {
+
+
+   private String name;
+   private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Aaaa{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
